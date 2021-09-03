e72a969ccd34258d1e07d7ef73e8d97ef7b227f6
facobackup <gustavomicaelbarbosa@gmail.com>
2021-09-03 13:00:46 -0300

    Tweaks

--- system/components/modules/dataset/DatasetManagement.js
+++ system/components/modules/dataset/DatasetManagement.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import styles from '../../styles/Dataset.module.css'
+import Cell from "./modules/Cell";
 
 export default function DatasetManagement(props) {
     return (
@@ -7,17 +8,13 @@ export default function DatasetManagement(props) {
             <table className={styles.table}>
                 <tr>
                     {props.data.dataset !== undefined && props.data.dataset.length > 0 ? Object.keys(props.data.dataset[0]).map(k => (
-                        <th contentEditable={true} style={{border: '#e0e0e0 1px solid', padding: '8px', borderCollapse: 'collapse'}}>
-                            {k}
-                        </th>
+                        <Cell value={k} asHeader={true}/>
                     )) : null}
                 </tr>
                 {props.data.dataset?.map(d => (
                     <tr>
                         {Object.keys(d).map(k => (
-                            <td contentEditable={true} style={{border: '#e0e0e0 1px solid', padding: '8px', borderCollapse: 'collapse'}}>
-                                {d[k]}
-                            </td>
+                            <Cell value={d[k]}/>
                         ))}
                     </tr>
                 ))}
